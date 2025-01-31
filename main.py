d876b59b6ed07318aa89873b6583a20cafd48775
Angel Vicente Gómez Aguiar <78941@alumnos.utleon.edu.mx>
2025-01-30 23:53:14 -0600

    Formularios

--- main.py
+++ main.py
@@ -1,38 +1,65 @@
-from flask import Flask
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 @app.route('/')
 def index():
-    return "Hola Mundo"
+    grupo = "IDGS803"
+    lista = ["Juan", "Pedro", "mario"]
+    return render_template('index.html', grupo=grupo, lista=lista)
 
-@app.route('/hola')
+@app.route('/OperasBas')
+def operas():
+    return render_template('OperasBas.html')
+
+
+@app.route('/resultado', methods=['GET', 'POST'])
+def resultado():
+    if request.method == "POST":
+        num1 = request.form.get("n1")
+        num2 = request.form.get("n2")
+        return "La suma de {} + {} es: {}".format(num1, num2, int(num1)+int(num2))
+
+@app.route('/ejemplo1')
+def ejemplo1():
+    return render_template('ejemplo1.html')
+
+@app.route('/ejemplo2')
+def ejemplo2():
+    return render_template('ejemplo2.html')
+
+@app.route("/hola")
 def hola():
-    return "Hola"
+    return "Hola!!!!"
 
-@app.route('/user/<string:user>')
-def user_profile(user):
-    return f"Hola {user}!!"
+@app.route("/user/<string:user>")
+def user(user):
+    return f"Hola {user}!!!"
 
-@app.route('/numero/<int:n>')
-def number(n):
-    return f"Numero: {n}!!!"
+@app.route("/numero/<int:n>")
+def numero(n):
+    return f"Numero {n}"
 
-@app.route('/user/<string:user>/<int:id>')
+@app.route("/user/<string:user>/<int:id>")
 def username(user, id):
-    return f"nombre: {user} ID: {id}"
+    return f"Nombre; {user} ID: {id}!!!"
 
-@app.route('/suma/<float:n1>/<float:n2>')
+@app.route("/suma/<float:n1>/<float:n2>")
 def suma(n1, n2):
-    return "La suma es: {}!!".format(n1 + n2)
+    return "La suma es: {}!!".format(n1+n2)
 
-@app.route('/default')
-@app.route('/default/<string.nom>')
-def func(nom='pedro'):
-    return "El nombre nom es" +nom
-
-
-
+@app.route("/form1")
+def form1():
+    return '''
+        <form>
+            <label>Nombre:</label>
+            <input type="text" name="nombre" placeholder="Nombre">
+            </br>
+            <label>Apellido</label>
+            <input type="text" name="nombre" placeholder="Nombre">
+            </br>
+        </form>
+    '''
 
 if __name__ == '__main__':
     app.run(debug=True)
